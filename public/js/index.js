6675126a3dddce15c7b93d07488c2f0b66ddfb2b
Nsikan <nsikane@live.co.uk>
2017-07-18 07:05:13 +0100

    Added error handling & changed api targets

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 /* eslint linebreak-style: ["error", "windows"]*/
 $(document).ready(function() {
-  let receivedLocation; 
+  let receivedLocation;
   let weatherInfo;
   let tempUnitState;
   //preloading all images
@@ -18,11 +18,11 @@ $(document).ready(function() {
   drizzle.src = "https://6oqrzq.dm2302.livefilestore.com/y3mY3MWYeJylxdBzh_x1my-PU8qD86Qc-SBeD8KUEq3Ul73isBgV5CQP0JFh6yk4Tk6EL4FxMD3C6bVT7o_gJ-Uu8IKQ9GlC24iY0MMO_niWXlyCCVKcVZwTiFXu3z0mIHA0beJKH4H2WOErmu1Ze2zokAPi0kUtPJKeV22t12JQQA?width=1920&height=1280&cropmode=none";
   let clear = new Image();
   clear.src = "https://6oqozq.dm2302.livefilestore.com/y3m7JrhTzNBodIy2F5_aR4Q6VXjlcjWeZuNPOrsQmGwS7i_Vx_0EBNduQ9Asgiz370tC9XzIPKt6KKKWi3Gx2Ka5Q3UtARyK2cpp8JEsa0Z8kEN4PTsNhccI0_eWofDz4EtMvHmQwhA6fKmvJrNb6aL2JELQ3zyHpTsbuiFkQCneSQ?width=1920&height=1080&cropmode=none";
-  let extreme = new Image();
+  const extreme = new Image();
   extreme.src = "https://6oqmzq.dm2302.livefilestore.com/y3mQYnh4t6apVgQm1i92ukqlk5kbP8OUFHOQONeA1QLTg8KFyo99YLpYT9bR2PnpOwkHK1xY7z4ik6zfkzz6I1dVnP8OctEbV9MPkkBieKx1Tlu-sTHWUtr3cezf4G3T4szJWgLj09aEHoaOJIU1Bd01wToJRWei7YKMKUzNqxb-D4?width=1920&height=1080&cropmode=none";
   let atmosphere = new Image();
-  atmosphere.src = "https://6oqnzq.dm2302.livefilestore.com/y3mdLw347Azb5KrG2_ZnRUC0T7kY57jDcJcPdxA5RveP1qM4w-xMfmeX_WlHSIkdLfmGXRe-KVZis_OOg1JtHDYiBcQhWclCv--wgvHX1QzhA6S0i1xoBaZyzbI8vFtfqoWS4cVFqqhRaSFeLy9PqXsbbvHuREXsr_7kGAmkMUSwOw?width=1920&height=1080&cropmode=none"; 
-  $("#giveMeWeather").on("click", function() {  
+  atmosphere.src = "https://6oqnzq.dm2302.livefilestore.com/y3mdLw347Azb5KrG2_ZnRUC0T7kY57jDcJcPdxA5RveP1qM4w-xMfmeX_WlHSIkdLfmGXRe-KVZis_OOg1JtHDYiBcQhWclCv--wgvHX1QzhA6S0i1xoBaZyzbI8vFtfqoWS4cVFqqhRaSFeLy9PqXsbbvHuREXsr_7kGAmkMUSwOw?width=1920&height=1080&cropmode=none";
+  $("#giveMeWeather").on("click", function() {
     getWeatherAndLocation("#cityAndCountryColn", "#weatherDescriptionColn", "#temperatureColn", "#changeUnitsButtonColn", "#weatherIconColn");
 
   });
@@ -86,12 +86,16 @@ $(document).ready(function() {
 
       })
       .done(function(data) {
-        receivedLocation = {
-          "lat": data.lat,
-          "lon": data.lon
-        };
-        //console.log(receivedLocation);   
-        getWeather(receivedLocation, locNode, descripNode, tempNode, chgBtnNode, iconNode);
+        if(data.error){
+          console.log(data);
+        } else {
+          receivedLocation = {
+            "lat": data.lat,
+            "lon": data.lon
+          };
+          //console.log(receivedLocation);
+          getWeather(receivedLocation, locNode, descripNode, tempNode, chgBtnNode, iconNode);
+        }
       })
       .fail(function() {
         //console.log("the status of the getLocation getJSON failed");
@@ -100,33 +104,32 @@ $(document).ready(function() {
   }
 
   function getWeather(whereToLook, locNode, descripNode, tempNode, chgBtnNode, iconNode) {
-    let apiTarget = "http://api.openweathermap.org/data/2.5/weather";
+    let apiTarget = "https://stormy-fortress-66721.herokuapp.com/dameelclima";
     let queryParams = whereToLook;
-    queryParams.APPID = "078ecb2576afa59e6e132d1ce4c68684";  
+    queryParams.APPID = "078ecb2576afa59e6e132d1ce4c68684";
     queryParams.units = "metric";
 
-    tempUnitState = "C"; //because the call is to metric, I assign state to C, so there's no confusion
-      
+    tempUnitState = 'C'; // because the call is to metric, I assign state to C, so there's no confusion
+
     $.getJSON(apiTarget, queryParams, function(data, textStatus, jqXHR) {
         //console.log("the status of the getLocation getJSON is " + textStatus);
       })
       .done(function(data) {
-        //console.log("the getWeather getJSON request succeeded");   
+        if(data.error){
+          console.log(data);
+        } else {
+          objOfHTML = htmlConv(jsonWeatherParser(data));
+          $(locNode).html(objOfHTML.cityAndCountry);    //access the appropriate prop in objofHTML
+          $(descripNode).html(objOfHTML.weatherDescription);
+          $(tempNode).html(objOfHTML.temperature);
+          $(chgBtnNode).html(objOfHTML.changeUnitsButton);
+          $('#changeUnits').bootstrapToggle(); //the toggle needs to be initialised for it to render properly
+          $(iconNode).html(objOfHTML.weatherIcon);
+          let backgroundURL = giveBkgrndUrlImgProp(objOfHTML.weatherMain);
 
-        //console.log(jsonWeatherParser(data));
-            //need to rewrite output for htmlConv
-        objOfHTML = htmlConv(jsonWeatherParser(data));
-        $(locNode).html(objOfHTML.cityAndCountry);    //access the appropriate prop in objofHTML
-        $(descripNode).html(objOfHTML.weatherDescription); 
-        $(tempNode).html(objOfHTML.temperature); 
-        $(chgBtnNode).html(objOfHTML.changeUnitsButton); 
-        $('#changeUnits').bootstrapToggle(); //the toggle needs to be initialised for it to render properly
-        $(iconNode).html(objOfHTML.weatherIcon); 
-        let backgroundURL = giveBkgrndUrlImgProp(objOfHTML.weatherMain);
-
-        $("body").css("background-image", backgroundURL);
-        $("#giveMeWeather").remove(); //take away the button after this has been used
-
+          $("body").css("background-image", backgroundURL);
+          $("#giveMeWeather").remove(); //take away the button after this has been used
+        }
       })
       .fail(function() {
         //console.log("the getWeather getJSON request failed");
@@ -149,11 +152,11 @@ $(document).ready(function() {
 
   }
 
-  function jsonWeatherParser(data) {  //Want to return an object with just temp and 
+  function jsonWeatherParser(data) {  //Want to return an object with just temp and
 
     let rootKeysIWant = ["weather", "main", "sys", "name"];
-    let outObj = nestedFilter(data, rootKeysIWant);   //console.log(outObj);    
-      
+    let outObj = nestedFilter(data, rootKeysIWant);   //console.log(outObj);
+
     return outObj;
 
   }
@@ -204,7 +207,7 @@ $(document).ready(function() {
     let cityAndCountry = '<h2 id="cityAndCountry" class="weatherInfo">' + toConvert.name + ', ' + toConvert.sys.country + '</h2>';
     let weatherDescription = '<h3 id="weatherDescription" class="weatherInfo">' + capitaliseEveryFirstLetter(toConvert.weather[0].description) + '</h3>';
     let iconURL = "http://openweathermap.org/img/w/" + String(toConvert.weather[0].icon) + ".png";
-    let weatherIcon = '<img src=' + iconURL + ' alt="an Icon of the current weather estara aqui"  id="weatherIcon" class="weatherInfo img-responsive center-block"/>';  
+    let weatherIcon = '<img src=' + iconURL + ' alt="an Icon of the current weather estara aqui"  id="weatherIcon" class="weatherInfo img-responsive center-block"/>';
     let changeUnitsButton = '<div class="outerToggle"><input type="checkbox" checked data-toggle="toggle" data-on="&deg;C" data-off="&deg;F" data-onstyle="primary" data-offstyle="info" data-style="android" id="changeUnits" data-size="large" ></div>';
     objToReturn.temperature = temperatureNode;
     objToReturn.cityAndCountry = cityAndCountry;
@@ -212,7 +215,7 @@ $(document).ready(function() {
     objToReturn.changeUnitsButton = changeUnitsButton;
     objToReturn.weatherIcon = weatherIcon;
     objToReturn.weatherMain = toConvert.weather[0].main;
-    return objToReturn;  
+    return objToReturn;
   }
 
   function myOneDecimalRnding(inputNumber) {
